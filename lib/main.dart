da4eb16981a72b6d073e2021193e3c88ad3eba3d
Kishan Kr Sharma <skishan781@gmail.com>
2020-07-23 19:23:58 +0530

    Challenge Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+        appBar: GradientAppBar(
+          title: Text('Dice'),
+          backgroundColorStart: Colors.cyan,
+          backgroundColorEnd: Colors.indigo,
         ),
         body: DicePage(),
       ),
@@ -15,9 +19,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
